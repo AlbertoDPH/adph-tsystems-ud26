1ba4a28d550caaca1ec1ab990bc255067a95180d
AlbertoDPH <114424982+AlbertoDPH@users.noreply.github.com>
2023-04-25 17:20:01 +0200

    Limpieza de imports que no se usaban
    Limpieza de imports que no se usaban en
    UD26-03
    UD26-03

--- UD26-Ejercicio2/src/main/java/com/delpozo/dto/Asignado.java
+++ UD26-Ejercicio2/src/main/java/com/delpozo/dto/Asignado.java
@@ -2,9 +2,6 @@ package com.delpozo.dto;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
--- UD26-Ejercicio3/src/main/java/com/delpozo/dto/Venta.java
+++ UD26-Ejercicio3/src/main/java/com/delpozo/dto/Venta.java
@@ -1,18 +1,11 @@
 package com.delpozo.dto;
 
-import java.util.List;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
